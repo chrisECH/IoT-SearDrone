f3761bd139848dd1d974664e7271d2167c3f2117
marapea <marapea0197@gmail.com>
2021-07-03 14:18:00 -0500

    v1

--- iot-env/Scripts/activate.bat
+++ iot-env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Archivos\Escuela\Semestre-10\IoT\proyFInal\iot-env
+set VIRTUAL_ENV=D:\11-.Semestre\IntCosas\Proyecto final\IoT-SearDrone\iot-env
 
 if not defined PROMPT set PROMPT=$P$G
 
